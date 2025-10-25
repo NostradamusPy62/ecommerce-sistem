6124ffdc1ba50ef1aafcb1f1148ba2fa20fa2de1
David Gomez <davgomez123t@gmail.com>
2025-10-25 19:49:04 -0300

    solucion nro ns cuanto de entry

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,10 @@
 # Salir inmediatamente si un comando falla
 set -e
 
+# Chequeo de configuración para producción
+echo "Ejecutando chequeo de despliegue de Django..."
+python manage.py check --deploy
+
 # Ejecutar las preparaciones de la base de datos y estáticos
 echo "Ejecutando collectstatic..."
 python manage.py collectstatic --noinput
