08c24fc3db284b889a6b8929b930ec77e99970ec
David Gomez <davgomez123t@gmail.com>
2025-10-25 18:09:52 -0300

    prueba de entry
    DEBUG: Habilita logging detallado en Gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,4 +12,4 @@ python manage.py migrate
 
 # Iniciar el servidor Gunicorn
 echo "Iniciando Gunicorn en el puerto $PORT..."
-gunicorn ecommerce.wsgi:application -w 1 --bind 0.0.0.0:$PORT
+gunicorn ecommerce.wsgi:application --bind 0.0.0.0:$PORT --workers 1 --log-level=debug --log-file=-
