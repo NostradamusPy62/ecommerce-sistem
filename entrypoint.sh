0bf3a46333ef75e495b012d17a238cfe6ca342ca
David Gomez <davgomez123t@gmail.com>
2025-10-25 19:40:10 -0300

    entrypoint "final"
    OJALA YA ANDE

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,5 +11,5 @@ echo "Aplicando migraciones de la base de datos..."
 python manage.py migrate
 
 # Iniciar el servidor Gunicorn
-echo "Iniciando Gunicorn en el puerto $PORT..."
-gunicorn ecommerce.wsgi:application --bind 0.0.0.0:$PORT --workers 1 --log-level=debug --log-file=-
+echo "Iniciando Gunicorn con 2 workers..."
+gunicorn ecommerce.wsgi:application --bind 0.0.0.0:$PORT --workers 2
