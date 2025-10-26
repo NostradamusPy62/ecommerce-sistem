c860a2d485f92984ecf9f44f2cbacefbbbda49b7
David Gomez <davgomez123t@gmail.com>
2025-10-25 22:26:21 -0300

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
-# Salir inmediatamente si un comando falla
 set -e
 
-# Ejecutar las preparaciones de la base de datos y estáticos
-echo "Ejecutando collectstatic..."
 python manage.py collectstatic --noinput
-
-echo "Aplicando migraciones de la base de datos..."
 python manage.py migrate
 
-# Iniciar el servidor uWSGI
-echo "Iniciando uWSGI..."
-exec uwsgi --ini uwsgi.ini
+echo "Iniciando Waitress en el puerto $PORT..."
+waitress-serve --port=$PORT ecommerce.wsgi:application
